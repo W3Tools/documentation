b637e7e8fd7750ba66f10060444ea9bbe21a97e2
W3Tools <ritter@w3tools.app>
2023-03-03 17:17:45 +0800

    Initial Commit

---
+++ docs/ton-usage.md
@@ -0,0 +1,28 @@
+---
+sidebar_position: 2
+---
+
+# Ton 钱包使用方法
+
+-   ### 安装钱包插件, 并切换到 Testnet
+
+    -   #### 下载 Chrome 插件: [MyTonWallet](https://chrome.google.com/webstore/detail/mytonwallet-%C2%B7-my-ton-wall/fldfpgipfncgndfolcbkdeeknbbbnhcc), 并按照提示生成一个钱包地址
+    -   #### [MyTonWallet](https://chrome.google.com/webstore/detail/mytonwallet-%C2%B7-my-ton-wall/fldfpgipfncgndfolcbkdeeknbbbnhcc)默认是主网, 切换到测试网步骤
+        -   #### 点击插件搜索栏旁边的 ![setting](./img//setting.png)设置按钮, 并点击设置
+        -   #### 找到版本号的位置 ![version](./img/version.png), 使用鼠标左键快速点击 5 次, 让其进入开发者模式
+        -   #### 弹出的开发者选项中 ![developer](./img/developer.png), 选择 `Testnet`即可
+
+-   ### 激活钱包
+
+    -   #### 在 ton 测试网 Testnet 中, 可以到 [Telegram](https://t.me/testgiver_ton_bot) 水龙头获取测试币
+    -   #### 领取到测试币之后, 在 [Testnet 的 tonscan](https://testnet.tonscan.org/) 中可以看到`余额`/`State`/`CONTRACT`. 其中 `State` 状态是 `Inactive`, `CONTRACT` 是`空内容`
+    -   #### 使用插件 MyTonWallet, 复制当前地址, 并选择`发送`, 给当前地址发送 0.001 的 ton
+    -   #### 交易确认之后, 在[Testnet 的 tonscan](https://testnet.tonscan.org/) 中可以看到`State` 状态是 `Active`, `Contract Type`版本是 `wallet v3 r2`, 并且在 `CONTRACT` 中填充了 `Bytecode` 和 `Raw data`
+    -   #### 至此, 钱包激活完毕! ton 钱包的激活条件是该地址需要创建一条交易, TVM 自动会补充该地址的合约内容. [参考说明](https://github.com/ton-community/tutorials/blob/main/01-wallet/index.md#:~:text=So%20when%20is,the%20first%20transfer.)
+
+-   ### 使用 ton SDK
+    -   #### 官方入门教程: [文档](https://ton-community.github.io/tutorials/01-wallet/)、[Github](https://github.com/ton-community/tutorials)、[个人整理的脚本](https://github.com/W3Tools/ton-contract)
+    -   #### Tips1: 官方教程中使用的 SDK 版本是 [WalletContractV4](https://github.com/ton-blockchain/wallet-contract/tree/v4r2-stable), 但是通过[MyTonWallet](https://chrome.google.com/webstore/detail/mytonwallet-%C2%B7-my-ton-wall/fldfpgipfncgndfolcbkdeeknbbbnhcc) 创建的地址是 `v3 r2`, 所以在 SDK 中使用的版本应该是 [WalletContractV3R2](https://github.com/ton-core/ton/blob/master/src/wallets/WalletContractV3R2.ts)
+    -   #### Tips2: 相同的助记词, 在不同版本上计算出的地址/私钥会不一样! 参考[说明](https://github.com/ton-community/tutorials/blob/main/01-wallet/index.md#step-5-wallets-contracts-have-versions)
+-   ### 交易失败, 在 ton scan 中提示的 Error Code 解释: https://ton.org/docs/learn/tvm-instructions/tvm-exit-codes
+-   ### FunC 在 vscode 的插件: `https://marketplace.visualstudio.com/items?itemName=tonwhales.func-vscode`
---
+++ docusaurus.config.js
@@ -0,0 +1,97 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'W3Tools Documentation',
+  tagline: 'Tutorials Summarized By W3Tools',
+  favicon: 'img/favicon.ico',
+  url: 'https://w3tools.app',
+  baseUrl: '/',
+  organizationName: 'W3Tools',
+  projectName: 'documentation',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'zh-Hans'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/W3Tools/documentation/tree/main/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'Home',
+        items: [
+          {
+            type: 'doc',
+            docId: 'main',
+            position: 'left',
+            label: 'Tutorial',
+          },
+          {
+            href: 'https://github.com/W3Tools/documentation',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/main',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'GitHub',
+                href: 'https://github.com/W3Tools/documentation',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} W3Tools Organization, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
